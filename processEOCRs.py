445ebe207a14607e6f94a042524d001f17a38386
Ellen K. Wilson <ekwilson@gmail.com>
2019-09-24 16:32:52 -0500

    fixed issue related to missing 961 field
    - If the file contains a record missing the 961 field, it writes an error to the text file of PO numbers and exports that record to a file of problem children
    
    - Also added date to file name for the list of POs and the file of problem children. All other POs should be unique to each file.

--- processEOCRs.py
+++ processEOCRs.py
@@ -1,34 +1,42 @@
-# This script goes through a file of EOCR records from YBP (in MARC format) and writes unique PO ids to a text file. It also splits the large file of records into smaller files with only one PO per file.
-# requires os, pymarc
+# This script goes through a file of EOCR records from YBP (in MARC format) and writes unique PO ids to a text file. It also splits the large file of records into smaller files with only one PO per file. If a record is missing a PO, it writes an error message to the text file and writes the records to a file.
 
-def extractPOs() :
-  import os
-  # get rid of an old PO file if present
-  if os.path.exists("POs.txt") :
-      os.remove("POs.txt")
-  nowname = "0"
-  with open(fname, "rb") as fh:
-    reader = MARCReader(fh)
-    # loop through records and find PO - write to file if it has not already been found and written.
-    for record in reader:
-        newname = record['961']['a']
-        if newname != nowname :
-            with open("POs.txt", "a") as fout:
-              fout.write(newname + "\n")
+# Requires datetime, pymarc
+
+def extractRecords():
+    nowname = "0"
+    with open(fname, "rb") as fh:
+        reader = MARCReader(fh)
+        for record in reader:
+            try :
+                newname = record['961']['a']
+                if newname != nowname :
+                    with open(newname + ".mrc", "ab") as out:
+                        out.write(record.as_marc())
+            except :
+                with open ("UL__NoPO-" + str(shortdate) + ".mrc", "ab") as out:
+                    out.write(record.as_marc())
         nowname = newname
 
-def splitEOCRS() :
-  with open(fname, "rb") as fh:
-    reader = MARCReader(fh)
-    for record in reader:
-        newname = record['961']['a']
-        nowname = newname
-        if newname == nowname :
-            with open(nowname + '.mrc', 'ab') as out:
-                  out.write(record.as_marc())
-        nowname = newname
+def extractPOs():
+    nowname = "0"
+    with open(fname, "rb") as fh:
+        reader = MARCReader(fh)
+        for record in reader:
+            try :
+                newname = record['961']['a']
+                if newname != nowname :
+                    with open("UL___POs-" + str(shortdate) + ".txt", "a") as fout:
+                        fout.write(newname + "\n")
+                nowname = newname
+            except :
+                with open("UL___POs-" + str(shortdate) + ".txt", "a") as fout:
+                    fout.write("***PO missing from record*** Record written to UL___POs-" + str(shortdate) + ".txt" + "\n")
 
 from pymarc import MARCReader
+from datetime import date
+
 fname = input("Enter file name: ")
+today = date.today()
+shortdate = today.strftime("%m-%d-%y")
+extractRecords()
 extractPOs()
-splitEOCRS()
